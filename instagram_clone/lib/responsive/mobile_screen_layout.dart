782933f2d980694dce9b646c4a2149ac1968fc06
ck-lamib <Groot@ck-lamib.local>
2022-11-15 08:11:09 +0545

    adding bottom nav bar

--- instagram_clone/lib/responsive/mobile_screen_layout.dart
+++ instagram_clone/lib/responsive/mobile_screen_layout.dart
@@ -1,9 +1,11 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/models/user.dart' as model;
-import 'package:instagram_clone/providers/user_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:instagram_clone/util/colors.dart';
+// import 'package:instagram_clone/models/user.dart' as model;
+// import 'package:instagram_clone/providers/user_provider.dart';
+// import 'package:provider/provider.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
@@ -13,31 +15,111 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
-  String userName = '';
+  int color = 0;
+  late PageController pageController;
+
   @override
   void initState() {
     super.initState();
-    getUserName();
-  }
-
-  void getUserName() async {
-    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
-    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-    DocumentSnapshot documentSnapshot =
-        await firebaseFirestore.collection('user').doc(firebaseAuth.currentUser!.uid).get();
-
-    debugPrint((documentSnapshot.data() as Map<String, dynamic>).toString());
-    setState(() {
-      userName = (documentSnapshot.data() as Map<String, dynamic>)['userName'];
-    });
+    pageController = PageController();
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      color = page;
+    });
+  }
+
+  // String userName = '';
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getUserName();
+  // }
+
+  // void getUserName() async {
+  //   FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  //   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  //   DocumentSnapshot documentSnapshot =
+  //       await firebaseFirestore.collection('user').doc(firebaseAuth.currentUser!.uid).get();
+
+  //   debugPrint((documentSnapshot.data() as Map<String, dynamic>).toString());
+  //   setState(() {
+  //     userName = (documentSnapshot.data() as Map<String, dynamic>)['userName'];
+  //   });
+  // }
+
+  @override
   Widget build(BuildContext context) {
-    model.User _user = Provider.of<UserProvider>(context).getUser;
+    // model.User _user = Provider.of<UserProvider>(context).getUser;
     return Scaffold(
-      body: Center(
-        child: Text(_user.photoUrl),
+      body: PageView(
+        children: [
+          Text("feed"),
+          Text("search"),
+          Text("add post"),
+          Text("notification"),
+          Text("profile"),
+        ],
+        controller: pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        onPageChanged: onPageChanged,
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: color == 0 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              color: color == 1 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_circle,
+              color: color == 2 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite,
+              color: color == 3 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              color: color == 4 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+        ],
+        onTap: navigationTapped,
       ),
     );
   }
